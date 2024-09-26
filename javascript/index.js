8c16724a4c895cfaa65d43d45d3723b146a8e54e
“Cindy <cindyvorjuela@outlook.com>
2024-09-26 16:49:42 -0500

    bodyy

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+
+
 const producto1= 70000;
 const producto2 = 20000;
 const producto3 = 80500;
@@ -5,34 +7,75 @@ const producto4 = 105800;
 const producto5 =38750;
 
 let total, cantidad, multiplicacion, suma,producto;
-producto=parseInt(prompt("Cual producto deseas: 1, 2, 3, 4 o 5? "));
+total=0;
+let seguir;
+while(true){
+    
+    mostrarProductos();
 
-switch(producto){
-    case 1:
-        cantidad=parseInt(prompt("Cuantas unidades quieres: "));
-        total=producto1*cantidad;
+    producto=parseInt(prompt("Cual producto deseas: 1, 2, 3, 4 o 5? "));
+    multiplicacion= function(precio,cantidad){
+        return precio*cantidad;
+    }
+    switch(producto){
+        case 1:
+            cantidad=parseInt(prompt("Cuantas unidades quieres: "));
+            console.log("Total producto 1: " + multiplicacion(producto1,cantidad));
+            total= total + multiplicacion(producto1,cantidad);
+            break;
+        case 2:
+            cantidad=parseInt(prompt("Cuantas unidades quieres: "));
+            console.log("Total producto 2: " + multiplicacion(producto2,cantidad));
+            total=total + multiplicacion(producto2,cantidad);
+            break;
+        case 3:
+            cantidad=parseInt(prompt("Cuantas unidades quieres: "));
+            console.log("Total producto 3: " + multiplicacion(producto3,cantidad));
+            total= total + multiplicacion(producto3,cantidad);
+            break;
+        case 4:
+            cantidad=parseInt(prompt("Cuantas unidades quieres: "));
+            console.log("Total producto 4: " + multiplicacion(producto4,cantidad));
+            total= total + multiplicacion(producto4,cantidad);
+            break;
+        case 5:
+            cantidad=parseInt(prompt("Cuantas unidades quieres: "));
+            console.log("Total producto 5: " + multiplicacion(producto5,cantidad));
+            total= total + multiplicacion(producto5,cantidad);
+
+            break;
+        default:
+            alert("No ingresaste una opción valida, intenta de nuevo, recuerda que debes poner solo el número del producto que quieres")
+    }
+
+    seguir=prompt("Deseas seguir tu compra? (ingresa 'si' para seguir o 'no' para seguir con el total ");
+
+    console.log(seguir.toLowerCase())
+
+    if (seguir.toLowerCase()==="no"){
         break;
-    case 2:
-        cantidad=parseInt(prompt("Cuantas unidades quieres: "));
-        total=producto2*cantidad;
-        break;
-    case 3:
-        cantidad=parseInt(prompt("Cuantas unidades quieres: "));
-        total=producto3*cantidad;
-        break;
-    case 4:
-        cantidad=parseInt(prompt("Cuantas unidades quieres: "));
-        total=producto4*cantidad;
-        break;
-    case 5:
-        cantidad=parseInt(prompt("Cuantas unidades quieres: "));
-        total=producto5*cantidad;
-        break;
-    default:
-        alert("No ingresaste una opción valida, intenta de nuevo")
+    }else if(seguir.toLowerCase() !== "si" && seguir.toLowerCase() !== "no"){
+        console.log("No ingresaste una opción valida, por lo tanto seguiras con la compra");
+    }
+    
+    
 }
-
 console.log("El valor de tu pedido es: "+ total);
+
+function mostrarProductos(){
+
+    const producto1= 70000;
+    const producto2 = 20000;
+    const producto3 = 80500;
+    const producto4 = 105800;
+    const producto5 =38750;
+    
+    console.log("Producto 1: " + producto1);
+    console.log("Producto 2: " + producto2);
+    console.log("Producto 3: " + producto3);
+    console.log("Producto 4: " + producto4);
+    console.log("Producto 5: " + producto5);
+}
 
 
 
