62e9eb4ff22bc0115e729a3b0cb8e9a0dc5ab366
“Cindy <cindyvorjuela@outlook.com>
2024-10-21 18:56:13 -0500

    ayo

--- javascript/index.js
+++ javascript/index.js
@@ -9,6 +9,79 @@ const producto5 =38750;
 let total, cantidad, multiplicacion, suma,producto;
 total=0;
 let seguir;
+productos = [
+    {
+        id: 1,
+        nombre: "producto 1",
+        
+        colorDisp: [
+            {
+                color: "rojo",
+                cantidad: 100,
+            },
+            {
+                color: "verde",
+                cantidad: 150,
+            },
+            {
+                color: "blanco",
+                cantidad: 50,
+            },
+            {
+                color: "negro",
+                cantidad: 400,
+            }
+        ]
+    },
+    {
+        id: 2,
+        nombre: "producto 2",
+        
+        colorDisp: [
+
+            {
+                color: "blanco",
+                cantidad: 35,
+            },
+            {
+                color: "negro",
+                cantidad: 20,
+            }
+        ]
+    },   
+    {
+        id: 3,
+        nombre: "producto 3",
+        
+        colorDisp: [
+            {
+                color: "morado",
+                cantidad: 500,
+            },
+            {
+                color: "gris",
+                cantidad: 200,
+            },
+            {
+                color: "blanco",
+                cantidad: 500,
+            },
+            {
+                color: "negro",
+                cantidad: 400,
+            }
+        ]
+    }
+];
+
+console.log(productos);
+
+function agregarProducto(){
+
+}
+function actualizarCantidad(cantidad, producto){
+
+}
 while(true){
     
     mostrarProductos();
